f97ba6c2502c0c178d2e72ab2ab489fddbc65e5a
MFurkanAR <58530400+m-furkan-a@users.noreply.github.com>
2021-11-23 12:36:11 +0300

    Add files via upload

---
+++ PratikGelismisHesapMakinesi/src/Main.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+
+public class Main {
+    static void plus() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 0, counter = 0;
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        counter = scan.nextInt();
+
+        for (int i =1 ; i <= counter ; i++){
+            System.out.print(i + ". Sayı : ");
+            number = scan.nextInt();
+            result += number;
+        }
+        System.out.println("Sonuç = " + result);
+    }
+
+    static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+        System.out.println("Sonuç = " + result);
+    }
+
+    static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, counter = 0;
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        counter = scan.nextInt();
+
+        for (int i =1 ; i <= counter ; i++){
+            System.out.print(i + ". Sayı : ");
+            number = scan.nextInt();
+            result *= number;
+        }
+        System.out.println("Sonuç = " + result);
+    }
+
+    static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("Böleni 0 giremezsiniz.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void mod(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. sayıyı giriniz :");
+        double a = scan.nextDouble();
+        System.out.print("2. sayıyı giriniz :");
+        double b = scan.nextDouble();
+
+        System.out.println("Sonuç : " + (a % b));
+    }
+
+    static void calc(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. sayıyı giriniz :");
+        int a = scan.nextInt();
+        System.out.print("2. sayıyı giriniz :");
+        int b = scan.nextInt();
+
+        System.out.println("Çevresi : " + (2*(a+b)));
+        System.out.println("Alanı : " + (a*b));
+    }
+        public static void main (String[]args){
+            Scanner input = new Scanner(System.in);
+            String menu = "1-Toplama İşlemi\n" +
+                    "2-Çıkarma İşlemi\n" +
+                    "3-Çarpma İşlemi\n" +
+                    "4-Bölme İşlmei\n" +
+                    "5-Üslü Sayı Hesaplama\n" +
+                    "6-Faktöriyel Hesaplama\n" +
+                    "7-Mod Alma\n" +
+                    "8-Dikdörtgen Alan ve Çevre Hesabı\n" +
+                    "0-Çıkış Yap";
+
+            int select;
+
+            System.out.println(menu);
+            do {
+                System.out.print("Lütfen Seçim Yapınız : ");
+                select = input.nextInt();
+
+                switch (select) {
+                    case 1:
+                        plus();
+                        break;
+                    case 2:
+                        minus();
+                        break;
+                    case 3:
+                        times();
+                        break;
+                    case 4:
+                        divided();
+                        break;
+                    case 5:
+                        power();
+                        break;
+                    case 6:
+                        factorial();
+                        break;
+                    case 7:
+                        mod();
+                        break;
+                    case 8:
+                        calc();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Yanlış ifade girdiniz. Tekrar Deneyin..");
+                }
+
+            } while (select != 0);
+        }
+    }
